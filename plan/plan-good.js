64632adadf1445d73bb4814a33f68dc2a13cfa4c
Leslie Caceda <lcaceda11@gmail.com>
2017-01-25 11:35:06 -0500

    updating otp url

--- plan/plan-good.js
+++ plan/plan-good.js
@@ -273,7 +273,7 @@ itinOpt = {
     title: "Trip Itinerary"
 };
 //////////////////////////////////////////
-var whitelabel_prefix = 'http://opentrip.atlantaregion.com/otp-rest-servlet/';
+var whitelabel_prefix = 'http://opentrip.atlantaregion.com/otp/routers/default/plan?';
 
 var whitelabel_minDate = new Date(2014, 02, 08);
 var whitelabel_maxDate = new Date(2020, 03, 30);
